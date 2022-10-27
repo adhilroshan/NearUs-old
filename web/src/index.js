41776379fc6383a230c21a245f34bcd3465cd4ba
adhilroshan <adhilchra@gmail.com>
2022-10-27 18:45:04 +0530

    express app

--- web/src/index.js
+++ web/src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./routes/routes";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <div>Hello world!</div>,
+//   },
+// ]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
---
+++ web/src/pages/chat/index.js
@@ -0,0 +1,12 @@
+import Map from "./components/Map";
+
+function Chat() {
+  return (
+    <div>
+      <Map />
+      <h1>This is the Chat page</h1>
+    </div>
+  );
+}
+
+export default Chat;
